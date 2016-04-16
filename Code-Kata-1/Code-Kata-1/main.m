d63c0427cb59c62cea7e031f948df38bbd78ae4e
Tom Williamson <tom@dtssoftware.com>
2016-04-15 21:52:39 -0400

    Initial

---
+++ Code-Kata-1/Code-Kata-1/main.m
@@ -0,0 +1,73 @@
+//
+//  main.m
+//  Code-Kata-1
+//
+//  Created by Tom Williamson on 4/15/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+BOOL pal(NSString *text);
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        //
+        //  find sum on digits divisable by 3 or 5
+        //
+ 
+        int total=0;
+        for (int i=1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0){
+                total += i;
+            }
+        }
+        
+        NSLog(@"Sum for numbers: %d", total);
+        
+        //
+        //  find largest palindrome
+        //
+        
+        int m1=0;
+        int m2=0;
+        const int max=999;
+        
+        for (int i=1; i <= max; i++) {
+            for (int j = i; j <= max; j++) {
+                if (pal([NSString stringWithFormat:@"%d", i*j])) {
+                    m1 = i;
+                    m2 = j;
+                }
+            }
+        }
+                    
+        NSLog(@"Palindrome : %d * %d = %d", m1, m2, m1*m2);
+        
+    }
+    
+    
+    return 0;
+                             
+}
+
+
+//
+//  see if passed string is a palindrome
+//
+
+BOOL pal(NSString *text) {
+
+    if (text.length <= 1)
+        return YES;
+    
+    else if ([text characterAtIndex:0] != [text characterAtIndex:text.length-1])
+        return NO;
+    
+    else
+        return(pal([text substringWithRange:NSMakeRange(1, text.length-2)]));
+    
+}
+
+
